f6a6711881d57d5f7fd5b79da8cfe9fcedc146ed
Brent Forrest <forrest.brent@gmail.com>
2020-09-30 10:13:51 +0900

    move

--- python/misc/steamedHams.py
+++ python/misc/steamedHams.py
@@ -4,14 +4,27 @@ class Person(object):
     def __init__(self):
         self.name = None
 
+class House(object):
+    def __init__(self, address):
+        self.address = address
+        self.onFire = False
+
+
 class NintendoChalmers(Person):
     def __init__(self):
         super(NintendoChalmers, self).__init__()
         self.name = "Superintendant Chalmers"
-    
-class Seymour(Person):
+
+    def warp(self, other, force=False)
+        obId = id(other)
+        if force:
+            self = other
+            other = SeymourSkinner()
+            
+
+class SeymourSkinner(Person):
     def __init__(self):
-        super(Seymour, self).__init__()
+        super(SeymourSkinner, self).__init__()
         self._name = "Armand Tanzarian"
         self.name = "Seymour Skinner"
         self.address = "330 Pikeland Ave, Springfield Oregon, USA"
@@ -23,6 +36,22 @@ class Seymour(Person):
         my_geo = results[0]["geometry"]["location"]
         return("Longitude:",my_geo["lng"],"\n","Latitude:",my_geo["lat"])
 
+luncheon = "Roast"
 
 class SteamedHams(object):
-    pass
+    Chalmers = NintendoChalmers()
+    Seymour = SeymourSkinner()
+    TheHouse = House(address = Seymour.address)
+    global luncheon
+
+    directions = Seymour.getDirections()
+    try:
+        Chalmers.goto(directions)
+    except Exception as e:
+        Chalmers.warp(Seymour, force=True)
+        print("I made it, despite %s" % e)
+
+    Seymour.speak(to=Chalmers, "greet")
+    Chalmers.react("myeh")
+    
+        
